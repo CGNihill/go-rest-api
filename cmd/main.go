6a8542975a49a590c72063a03f8810307719ea3f
CGNihill <dimcoj5@gmail.com>
2022-08-28 13:17:59 +0300

    new

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 
 	todo "github.com/CGNihill/go-rest-api"
+	"github.com/CGNihill/go-rest-api/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while runing http server: %s", err.Error())
 	}
 }
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -8,27 +8,27 @@ func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 	auth := router.Group("/auth")
 	{
-		auth.POST("/sign-in")
-		auth.POST("/sign-up")
+		auth.POST("/sign-in", h.signIn)
+		auth.POST("/sign-up", h.signUp)
 	}
 
 	api := router.Group("/api")
 	{
 		list := api.Group("/lists")
 		{
-			list.POST("/")
-			list.GET("/")
-			list.GET("/:id")
-			list.POST("/:id")
-			list.DELETE("/:id")
+			list.POST("/", h.createList)
+			list.GET("/", h.getAllLists)
+			list.GET("/:id", h.getListById)
+			list.POST("/:id", h.updateList)
+			list.DELETE("/:id", h.deleteList)
 
 			items := list.Group(":id/items")
 			{
-				items.POST("/")
-				items.GET("/")
-				items.GET("/:item_id")
-				items.POST("/:item_id")
-				items.DELETE("/:item_id")
+				items.POST("/", h.createItem)
+				items.GET("/", h.getAllItems)
+				items.GET("/:item_id", h.getItemById)
+				items.POST("/:item_id", h.updateItem)
+				items.DELETE("/:item_id", h.deleteItem)
 			}
 		}
 	}
